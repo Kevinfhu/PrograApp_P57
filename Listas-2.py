d9dd5620dbddf9b03c774283feac80add434a619
Kevinfhu <74988078+Kevinfhu@users.noreply.github.com>
2021-01-27 08:55:57 -0500

    Create Listas-2.py

---
+++ Listas-2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 14:13:51 2020
+
+@author:  Kevin Fausto 
+"""
+lista=["R1",5.5,6,True,"R2"]    
+print(lista[1])
+print(lista[-1])
+print(lista[-5])
+del lista[4]
+print (lista)
+lista[1]="R3"
+print (lista)
