4e74969bdefce3e4f5754f367e8c892faa23582c
Kevinfhu <74988078+Kevinfhu@users.noreply.github.com>
2021-01-27 08:53:34 -0500

    Create While_true.py

---
+++ While_true.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  5 15:25:50 2021
+
+@author: ♫♪ Kevin Fausto ♪♫
+"""
+
+x=input('Enter a number to count to: ')
+x=int(x)
+y=1
+while True:
+    print (y)
+    y=y+1
+    if y>x:
+        break
