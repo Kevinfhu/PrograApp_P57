b097782e20986d8c9f09933a1501c03bfb2e0c2c
Kevinfhu <74988078+Kevinfhu@users.noreply.github.com>
2021-01-27 08:50:27 -0500

    Create Direction_lista_array.py

---
+++ Direction_lista_array.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  8 14:31:26 2020
+
+@author: ♫♪ Kevin Fausto ♪♫
+"""
+
+dict1={"R1":"192.168.0.1","R2":"192.168.0.2","R3":"192.168.0.3"}
+print(dict1)
+print(dict1['R2'])
+dict1['R4']="192.168.0.4"
+dict1['S3']="192.168.1.3"
+print("S3" in dict1)
