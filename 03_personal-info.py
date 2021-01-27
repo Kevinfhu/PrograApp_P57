70001c786f6071ebc5d48a05bc0c28fc6ad36a91
Kevinfhu <74988078+Kevinfhu@users.noreply.github.com>
2021-01-27 08:41:04 -0500

    Create 03_personal-info.py

---
+++ 03_personal-info.py
@@ -0,0 +1,12 @@
+"""
+Created on Tue Dec 22 14:26:46 2020
+@author: ♫♪ Kevin Fausto ♪♫
+"""
+print("Ingrese los datos")
+space=" "
+nombre=input('Ingrese nombre:')
+apellido=input('Ingrese apellido:')
+locali=input('Ingrese localizacion:')
+edad=input('Ingrese edad:')
+space=" "
+print("Bienvenido:",nombre,apellido,space,"Su edad es:",edad,space,"Se encuentra en:",locali)
