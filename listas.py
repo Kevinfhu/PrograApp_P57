c971bf0efebee013a68f0f9a1f64d5abfd021d73
Kevinfhu <74988078+Kevinfhu@users.noreply.github.com>
2021-01-27 08:52:03 -0500

    Create listas.py

---
+++ listas.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  5 14:26:31 2021
+
+@author: ♫♪ Kevin Fausto ♪♫
+"""
+
+lista1=['R1','R2','R3','S1','S2','S3']
+for i in lista1:
+    if 'S' in i:
+      print(i)
+
